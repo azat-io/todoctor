40a45f5c0a91da894d7c46a666b35a92d00c5289
Azat S. <to@azat.io>
2024-10-23 01:46:47 +0300

    fix: add missed bin script extension

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,11 +2,14 @@ import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import fs from 'node:fs'
 
-import { binaries } from '../bin/todoctor'
+import { binaries } from '../bin/todoctor.js'
 
 let filename = fileURLToPath(import.meta.url)
 let dirname = path.dirname(filename)
 
 Object.values(binaries).forEach(binaryPath => {
-  fs.chmodSync(path.join(dirname, '../bin/', binaryPath), 0o755)
+  let fullBinaryPath = path.join(dirname, '../bin/', binaryPath)
+  if (fs.existsSync(fullBinaryPath)) {
+    fs.chmodSync(path.join(dirname, '../bin/', binaryPath), 0o755)
+  }
 })
